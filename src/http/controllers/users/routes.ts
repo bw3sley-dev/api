14df09927bc5cf978e93009b61ee0e43e9e64de6
bw3sley <bw3sley@gmail.com>
2024-06-10 09:27:41 -0300

    ✅ fix: remove unused imports

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,7 +2,6 @@ import type { FastifyInstance } from 'fastify'
 
 import { register } from './register'
 import { profile } from './profile'
-import { credentials } from './credentials'
 
 import { updateEmail } from './update-email'
 import { updatePassword } from './update-password'
@@ -13,7 +12,6 @@ export async function userRoutes(app: FastifyInstance) {
   app.post('/users', register)
 
   app.get('/me', { onRequest: [verifyJWT] }, profile)
-  app.get('/credentials', { onRequest: [verifyJWT] }, credentials)
 
   app.patch('/update-email', { onRequest: [verifyJWT] }, updateEmail)
   app.patch('/update-password', { onRequest: [verifyJWT] }, updatePassword)
