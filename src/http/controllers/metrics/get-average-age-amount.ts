46946191335ff7e5caf5ff9f76454951a7809ac5
Wesley Bernardes <bw3sley@gmail.com>
2024-08-10 12:22:48 -0300

    ✅: fix: average athletes age number precision

--- src/http/controllers/metrics/get-average-age-amount.ts
+++ src/http/controllers/metrics/get-average-age-amount.ts
@@ -40,7 +40,7 @@ export async function getAverageAgeAmount(
     const amount = totalAge / athletes.length
 
     return reply.send({
-      amount,
+      amount: Number(amount.toFixed(2)),
     })
   }
 
